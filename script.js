d8ff4d4a8d8e52ad6269f2c1a0eabbecd83fccc4
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-28 20:03:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 //your JS code here. If required.
+function showWindowSize(event){
+	let width = document.getElementById("width");
+	let height = document.getElementById("height");
+	width.innerText = window.screen.width;
+	height.innerText = window.screen.height;
+}
+window.onresize = showWindowSize;
