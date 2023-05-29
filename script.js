39cb01975ce8c8690d4e30c4b213df81ba075957
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-29 23:45:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //your JS code here. If required.
 const div = document.getElementById("sizeInfo");
-document.body.append("div");
+document.body.append(div);
 //div.append(size);
 function showWindowSize(event){
 	size.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
