5b6526c22a1bc9c056c2b7991cd05c39ef37895e
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-05-29 23:56:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 //your JS code here. If required.
-const div = document.getElementById("sizeInfo");
-document.body.append(div);
+const h1 = document.getElementById("sizeInfo");
+document.body.append(h1);
 //div.append(size);
 function showWindowSize(){
-	div.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+	h1.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`
 }
 window.onresize = showWindowSize;
