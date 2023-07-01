ca742e49def966d01fad91e3c9db68b9e07d7f86
Siddharth Kishan <91934415+sidkishan@users.noreply.github.com>
2023-07-01 09:49:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,8 +2,6 @@
 const screenSize = document.getElementById("sizeInfo")
 screenSize.innerText = `Width: ${window.innerWidth} and Height: ${window.innerHeight}`;
 
-document.body.append(screenSize);
-
 // onresize is a property that takes a function value. which is why
 
 window.onresize = function(){
